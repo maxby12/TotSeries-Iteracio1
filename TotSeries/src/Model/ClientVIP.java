5b7d7871deba8443e449d13ed2768c97d5f01c1a
Raül <Ral@Raul-PC>
2016-11-26 12:31:38 +0100

    del pal afegir nosek

---
+++ TotSeries/src/Model/ClientVIP.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Raül
+ */
+public class ClientVIP extends Client {
+
+    @Override
+    public String toString() {
+        return "ClientVIP{" + '}';
+    }
+
+    public ClientVIP() {
+    }
+    
+}
+
+
