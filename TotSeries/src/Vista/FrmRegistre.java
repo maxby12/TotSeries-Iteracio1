1c5345b383f8bb09d7ea561fca317809e36321d2
Albert <agarcisa81@alumnes.ub.edu>
2016-12-07 18:15:18 +0100

    Dia, mes i any restringit a valors enters.

--- TotSeries/src/Vista/FrmRegistre.java
+++ TotSeries/src/Vista/FrmRegistre.java
@@ -7,6 +7,11 @@ package Vista;
 
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
 
 /**
  *
@@ -30,6 +35,12 @@ public class FrmRegistre extends javax.swing.JDialog {
         this._canviAny = false;
         initComponents();
         
+        PlainDocument doc1 = (PlainDocument) txtDia.getDocument();
+        doc1.setDocumentFilter(new MyIntFilter());
+        PlainDocument doc2 = (PlainDocument) txtMes.getDocument();
+        doc2.setDocumentFilter(new MyIntFilter());
+        PlainDocument doc3 = (PlainDocument) txtAny.getDocument();
+        doc3.setDocumentFilter(new MyIntFilter());
         
         txtUser.getDocument().addDocumentListener(new DocumentListener() {
 
@@ -367,4 +378,68 @@ public class FrmRegistre extends javax.swing.JDialog {
     private javax.swing.JTextField txtPas;
     private javax.swing.JTextField txtUser;
     // End of variables declaration//GEN-END:variables
+}
+
+
+
+
+class MyIntFilter extends DocumentFilter {
+   @Override
+   public void insertString(FilterBypass fb, int offset, String string,
+         AttributeSet attr) throws BadLocationException {
+
+      Document doc = fb.getDocument();
+      StringBuilder sb = new StringBuilder();
+      sb.append(doc.getText(0, doc.getLength()));
+      sb.insert(offset, string);
+
+      if (test(sb.toString())) {
+         super.insertString(fb, offset, string, attr);
+      } else {
+         // warn the user and don't allow the insert
+      }
+   }
+
+   private boolean test(String text) {
+      if (text.equals("")) return true;
+      try {
+         Integer.parseInt(text);
+         return true;
+      } catch (NumberFormatException e) {
+         return false;
+      }
+   }
+
+   @Override
+   public void replace(FilterBypass fb, int offset, int length, String text,
+         AttributeSet attrs) throws BadLocationException {
+
+      Document doc = fb.getDocument();
+      StringBuilder sb = new StringBuilder();
+      sb.append(doc.getText(0, doc.getLength()));
+      sb.replace(offset, offset + length, text);
+
+      if (test(sb.toString())) {
+         super.replace(fb, offset, length, text, attrs);
+      } else {
+         // warn the user and don't allow the insert
+      }
+
+   }
+
+   @Override
+   public void remove(FilterBypass fb, int offset, int length)
+         throws BadLocationException {
+      Document doc = fb.getDocument();
+      StringBuilder sb = new StringBuilder();
+      sb.append(doc.getText(0, doc.getLength()));
+      sb.delete(offset, offset + length);
+      
+      if (test(sb.toString())) {
+         super.remove(fb, offset, length);
+      } else {
+         // warn the user and don't allow the insert
+      }
+
+   }
 }
