59f03ed04da3118f7cc46edd7e65624acb78900a
Albert <agarcisa81@alumnes.ub.edu>
2016-12-07 19:58:18 +0100

    Controlador amb Singleton + aplicació creada des de controlador

--- TotSeries/src/Controlador/TotSeries.java
+++ TotSeries/src/Controlador/TotSeries.java
@@ -9,6 +9,7 @@ import Model.Administrador;
 import Model.Cataleg;
 import Model.Client;
 import Model.TotSeriesDades;
+import Vista.MenuInicial;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,20 +19,73 @@ import java.util.Date;
  */
 public class TotSeries {
     
+    private static TotSeries _ctrl;
     private TotSeriesDades _data;
     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        
+        final TotSeries controlador = TotSeries.getInstance();
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MenuInicial main = new MenuInicial(controlador);
+                main.setVisible(true);
+            }
+        });
+    }
     
-    public TotSeries(TotSeriesDades data, String rutaXML) {
-        this._data = data;
+    private TotSeries() {
+        // Creem data actual i calculem el mes següent, serà la data de pagament
+        Date dataPagament = new Date();
+        dataPagament.setMonth(dataPagament.getMonth()+1);
+        // Creem el Model amb la data de pagament
+        this._data = new TotSeriesDades(dataPagament);
+        
         TotSeriesDataManager dataManager = new TotSeriesDataManager();
-        dataManager.obtenirDades(rutaXML);
+        dataManager.obtenirDades("data/TotSeries.xml");
         ArrayList<Client> clients = dataManager.getLlistaClients();
         ArrayList<Administrador> admins = dataManager.getAdministradors();
         Cataleg c = dataManager.getCataleg();
         this._data.setCataleg(c);
         this._data.setAdministradors(admins);
         this._data.setLlistaClients(clients);
-        
+    }
+    
+    public static TotSeries getInstance() {
+        if (_ctrl == null)
+            synchronized(TotSeries.class) {
+                if (_ctrl== null)
+                    _ctrl = new TotSeries();
+            }
+        return _ctrl; 
     }
     
     // METODES DE CASSOS D'US
--- TotSeries/src/Vista/MenuInicial.java
+++ TotSeries/src/Vista/MenuInicial.java
@@ -5,18 +5,30 @@
  */
 package Vista;
 
+import Controlador.TotSeries;
+
 /**
  *
  * @author mat.aules
  */
 public class MenuInicial extends javax.swing.JFrame {
-
+    
+    
+    private TotSeries _ctrl;
+    
+    
     /**
      * Creates new form MenuInicial
      */
     public MenuInicial() {
         initComponents();
     }
+
+    public MenuInicial(TotSeries controlador) {
+        initComponents();
+        this._ctrl = controlador;
+    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -109,40 +121,6 @@ public class MenuInicial extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnRegActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MenuInicial().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCat;
